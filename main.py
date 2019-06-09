82711a8e79fe306edaede1646708565ead5a870f
Renan Venancio <renan_1419@hotmail.com>
2019-06-09 19:54:09 -0300

    Finalizando o projeto

--- main.py
+++ main.py
@@ -1,20 +1,68 @@
-comp = int(input('Digite o comprimento do cartão em caracateres: '))
-if(comp >= 8 and comp <=1000):
-    print('ok')
-else:
-    print('Tam invalido')
+sair = False
+nrTetse = 0
+while sair == False:
 
-#Pegando a quantidade de frases
-frColetadas = int(input('Quantidade de frases coletadas: '))
+    entradaValida = False
+    while entradaValida == False:
 
-frDados = []
-for i in range(frColetadas):
-    print(8 * '*' + 'FRASE ' + str(i+1) + 8 * '*')
-    numCaracteres = int(input('Quantidade de caracteres da frase: '))
-    numExp = int(input('Quantidade de expressões: '))
-    frDados.append(list([numCaracteres, numExp]))
-    print(20 * '*')
+        maxCarac = int(input('Quantidade de caracteres do cartão->'))
+        qtdFrases = int(input('Quantidade de frases->'))
+        if(maxCarac >= 8 and maxCarac <=1000) and (qtdFrases >= 1 and qtdFrases <=50):
+            entradaValida = True
+        else:
+            print('Entradas inválidas\n C precisa ser >= 8 e <= 1000\n F precisa ser > 1 e <=50\n TENTE NOVAMENTE!')
 
 
-for i in frDados:
-    print(str(i[0]) + '|' + str(i[0]))
+
+    frDados = []
+    for i in range(qtdFrases):
+        print(8 * '*' + 'FRASE ' + str(i+1) + 8 * '*')
+
+        dadosValidos = False
+        while dadosValidos == False:
+
+            numCaracteres = int(input('Quantidade de caracteres da frase: '))
+            numExp = int(input('Quantidade de expressões: '))
+
+            if(numCaracteres >= 8 and numCaracteres <= 200) and (numExp >= 1 and numExp <=25):
+                dadosValidos = True
+                frDados.append([numCaracteres, numExp])
+            else:
+                print('Entradas inválidas\n A Quantidade de caracteres da frase precisa ser >= 8 e <= 200\n Quantidade de expressões precisa ser > 1 e <=25\n TENTE NOVAMENTE!')
+
+        print(23 * '*')
+
+    def estrategiaGulosa(dados):
+        caracteres = maxCarac
+        solucao = 0
+        parar = False
+        maior = 0
+        maiorIndex = -1
+
+        while parar == False:
+
+            for i in range(len(dados)):
+                if(dados[i][1] > maior):
+                    maior = dados[i][1]
+                    maiorIndex = [i][0]
+                    maiorCacteres = dados[i][0]
+
+            if(maiorIndex > -1):
+                del(dados[maiorIndex])
+                maiorIndex = -1
+
+
+            if(caracteres - maiorCacteres) >= 0:
+                caracteres -= maiorCacteres
+                solucao += maior
+                maior = 0
+            else:
+                parar = True
+
+        return solucao
+
+    nrTetse += 1
+
+
+    print('Teste ' + str(nrTetse))
+    print('Solução Gulosa: ' + str(estrategiaGulosa(frDados)) + '\n')
